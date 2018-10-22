74a42f32899d3d225cede2f37690d3bb5b72d7ef
jgch88 <radium.laboratories@gmail.com>
2018-10-23 01:02:34 +0800

    done section 2.4.1

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	public enum RotationAxes {
+		MouseXandY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+	public RotationAxes axes = RotationAxes.MouseXandY;
+	public float sensitivtyHorizontal = 9.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			// Unity Input API
+			// https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+			transform.Rotate (0, Input.GetAxis("Mouse X"), 0);
+		} else if (axes == RotationAxes.MouseY) {
+		} else {
+		}
+	}
+}
