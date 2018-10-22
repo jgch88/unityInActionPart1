353490d7d61ec4c0656e2c05bf79a04f187418e4
jgch88 <radium.laboratories@gmail.com>
2018-10-23 02:06:14 +0800

    done section 2.5.3

--- Assets/KeyboardMovement.cs
+++ Assets/KeyboardMovement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class KeyboardMovement : MonoBehaviour {
 	public float speed = 3.0f;
 
+	private CharacterController _charController;
 	// Use this for initialization
 	void Start () {
-		
+		// access the CharacterController component attached to this object
+		// to utilize its collision detection behaviour
+		_charController = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
@@ -16,8 +19,15 @@ public class KeyboardMovement : MonoBehaviour {
 		// "Horizontal/Vertical" is an indirect abstract name for keyboard mapping WASD
 		float deltaZ = Input.GetAxis ("Vertical") * speed;
 
+		Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed); // because moving diagonally shouldn't be faster than moving forward
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement); // transform movement from local to global coordinates -> this gives rise to "strafing" behaviour
+		// CharacterController's Move() API which detects collisions
+		_charController.Move (movement);
+
 		// Time.deltaTime is the amount of time between frames
 		// This makes it "frame rate independent" (for different GPU/FPS speeds)
-		transform.Translate (deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+		// transform.Translate (deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
 	}
 }
