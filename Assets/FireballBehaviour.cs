e81cfe53d1a20cbf9c0388df636cf333aac394cf
jgch88 <radium.laboratories@gmail.com>
2018-10-23 15:36:58 +0800

    done section 3.5.2

---
+++ Assets/FireballBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballBehaviour : MonoBehaviour {
+	public float speed = 3.0f;
+	public int damage = 1;
+	
+	// Which sequence of scripts Updates() in the engine?
+	// There is Edit -> Project Settings -> Script Execution Order
+	void Update () {
+		transform.Translate (0, 0, speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Fireball Collision");
+		PlayerBehaviour player = other.GetComponent<PlayerBehaviour> ();
+		if (player != null) {
+			Debug.Log("Player hit");
+		}
+		Destroy(this.gameObject);
+	}
+
+}
--- Assets/WanderingAIBehaviour.cs
+++ Assets/WanderingAIBehaviour.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WanderingAIBehaviour : MonoBehaviour {
+
+	[SerializeField] private GameObject fireballPrefab;
 	public float speed = 2.0f;
 	public float obstacleRange = 0.5f;
+	private GameObject _fireball;
 
 	// We need this variable to prevent AI from moving when it's "dead"
 	private bool _isAlive; // we should use Finite State Machines (or state design pattern) rather than
@@ -26,7 +29,17 @@ public class WanderingAIBehaviour : MonoBehaviour {
 		RaycastHit hit;
 		// Spherecast has a "width", whereas a raycast is an infinitely thin ray
 		if (Physics.SphereCast (ray, 0.75f, out hit)) {
-			if (hit.distance < obstacleRange) {
+			GameObject hitObject = hit.transform.gameObject;
+			if (hitObject.GetComponent<PlayerBehaviour> ()) {
+				if (_fireball == null) {
+					_fireball = Instantiate (fireballPrefab) as GameObject;
+					// place it in front of enemy
+					_fireball.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
+					// point it in the same direction as enemy (this.transform.rotation has this omitted)
+					_fireball.transform.rotation = transform.rotation;
+				}
+			}
+			else if (hit.distance < obstacleRange) {
 				// rotate to a semi-random new direction
 				float angle = Random.Range (-110, 110);
 				transform.Rotate (0, angle, 0);
