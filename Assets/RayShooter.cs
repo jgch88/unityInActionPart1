62b37a7f2b0b9607e0d8bf1b4d8720e8e2e94c9e
jgch88 <radium.laboratories@gmail.com>
2018-10-23 12:56:18 +0800

    done section 3.1.2

---
+++ Assets/RayShooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour {
+	private Camera _camera;
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 screenCenterPoint = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+			// create ray from center of the camera screen
+			Ray ray = _camera.ScreenPointToRay (screenCenterPoint);
+			RaycastHit hit; // RaycastHit data structure stores information about the intersection of the ray
+			// Needs Physics.Raycast() to mutate data on where the intersection happened and which object was intersected
+
+			// the out in the argument is like C++ passing in by reference, as opposed to copying it into scope?
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log ("Hit " + hit.point);
+			} // else if Physics.Raycast returns false, it didn't hit any object (e.g. it hit the "sky")
+		}
+	}
+}
