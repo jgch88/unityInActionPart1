e6ca3516146838198a31578b16cee82d7540208a
jgch88 <radium.laboratories@gmail.com>
2018-11-02 10:39:59 +0800

    done section 9.3.1, 9.3.2, 9.3.3

--- Assets/CollectibleItem.cs
+++ Assets/CollectibleItem.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CollectibleItem : MonoBehaviour {
 	[SerializeField] private string itemName;
 
-	void OnTriggerEnter () {
+	void OnTriggerEnter (Collider other) {
+		Managers.Inventory.AddItem (itemName);
 		Debug.Log ("Item collected: " + itemName);
 		Destroy (this.gameObject);
 	}
---
+++ Assets/Managers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// manager of managers!
+[RequireComponent(typeof(PlayerManager))]
+[RequireComponent(typeof(InventoryManager))]
+
+public class Managers : MonoBehaviour {
+	public static PlayerManager Player { get; private set; }
+	public static InventoryManager Inventory { get; private set; }
+
+	private List<IGameManager> _startSequence;
+
+	void Awake () {
+		Player = GetComponent<PlayerManager> ();
+		Inventory = GetComponent<InventoryManager> ();
+
+		_startSequence = new List<IGameManager> ();
+		_startSequence.Add (Player);
+		_startSequence.Add (Inventory);
+
+		StartCoroutine (StartupManagers ());
+	}
+
+	private IEnumerator StartupManagers() {
+		foreach (IGameManager manager in _startSequence) {
+			manager.Startup ();
+		}
+
+		yield return null;
+
+		int numModules = _startSequence.Count;
+		int numReady = 0;
+
+		// enter a loop to keep checking whether the managers
+		// have started up and not proceed until all have (stuck in the while loop)
+		while (numReady < numModules) {
+			int lastReady = numReady;
+			numReady = 0;
+
+			foreach (IGameManager manager in _startSequence) {
+				if (manager.status == ManagerStatus.Started) {
+					numReady++;
+				}
+			}
+
+			if (numReady > lastReady)
+				Debug.Log ("Progress: " + numReady + "/" + numModules);
+			yield return null;
+		}
+
+		Debug.Log ("All managers started up");
+	}
+}
